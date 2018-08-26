3551b3784ab725e0a2408aa9210ec43f885beab8
pau-rasel <sydul.hassan@primeasia.edu.bd>
2018-08-26 15:33:03 +0600

    Controllers,Services are modified to handle exception

--- PauFacultyPortal.Server/Controllers/SectionController.cs
+++ PauFacultyPortal.Server/Controllers/SectionController.cs
@@ -16,51 +16,38 @@ namespace PauFacultyPortal.Server.Controllers
         SectionService service = new SectionService();
 
         [HttpGet]
-        public List<SectionListViewModel> Get(int SemesterID)
+        public HttpResponseMessage Get(int SemesterID)
         {
-            //ResponseModel response = new ResponseModel();
-            //try
-            //{
-            var loginId = ((ClaimsIdentity)User.Identity).FindFirst("LoginID").Value;
-            List<SectionListViewModel> models = service.GetSections(SemesterID, loginId);
-            // response = new ResponseModel(models, true, "", null);
-
-            //}
-            //catch (Exception exception)
-            //{
-
-            //    response = new ResponseModel(null, false, "Error Found", exception);
-            //}
-
-
-            //return Ok(response);
-            return models;
+            try
+            {
+                var loginId = ((ClaimsIdentity)User.Identity).FindFirst("LoginID").Value;
+                List<SectionListViewModel> models = service.GetSections(SemesterID, loginId);
+                return models.Count > 0 ? Request.CreateResponse(HttpStatusCode.OK, models) : Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                    "Section does not exist");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
         }
 
         [HttpGet]
-        public List<SectionStudentsViewModel> GetSectionStudent(int SectionID)
+        public HttpResponseMessage GetSectionStudent(int SectionID)
         {
+            try
+            {
+                var loginId = ((ClaimsIdentity)User.Identity).FindFirst("LoginID").Value;
+                List<SectionStudentsViewModel> models = service.GetSectionWiseStudents(SectionID, loginId);
+                return models.Count > 0 ? Request.CreateResponse(HttpStatusCode.OK, models) : Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                   "No Student Found");
 
+            }
+            catch (Exception ex)
+            {
 
-            //ResponseModel response = new ResponseModel();
-            //try
-            //{
-            // List<ProfileViewModel>
-
-            var loginId = ((ClaimsIdentity)User.Identity).FindFirst("LoginID").Value;
-            List<SectionStudentsViewModel> models = service.GetSectionWiseStudents(SectionID, loginId);
-            // response = new ResponseModel(models, true, "", null);
-
-            //}
-            //catch (Exception exception)
-            //{
-
-            //    response = new ResponseModel(null, false, "Error Found", exception);
-            //}
-
-
-            //return Ok(response);
-            return models;
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
 
         }
 
--- PauFacultyPortal.Service/Auth/AuthService.cs
+++ PauFacultyPortal.Service/Auth/AuthService.cs
@@ -12,34 +12,37 @@ namespace PauFacultyPortal.Service.Auth
 {
     public class AuthService
     {
-        PauFacultyPortalEntities _db = new PauFacultyPortalEntities();
+        //PauFacultyPortalEntities _db = new PauFacultyPortalEntities();
+
 
         public List<UserViewModel> GetUserList()
         {
             List<UserViewModel> list = new List<UserViewModel>();
-
-            var result = from act in _db.Accounts.Where(x => x.AccountsRoleId == 1 && x.AccountsRoleId == 5 && x.Deactivate == false)
-                         select new
-                         {
-                             LoginIdentity = act.LoginIdentity,
-                             Password = act.Password,
-                             Name = act.Name,
-                             Email = act.Email
-                         };
-
-            foreach (var item in result.ToList())
+            using (var db = new PauFacultyPortalEntities())
             {
+                var result = from act in db.Accounts.Where(x => x.AccountsRoleId == 5 && x.Deactivate == false)
+                             select new
+                             {
+                                 LoginIdentity = act.LoginIdentity,
+                                 Password = act.Password,
+                                 Name = act.Name,
+                                 Email = act.Email
+                             };
 
-                var model = new UserViewModel()
+                foreach (var item in result.ToList())
                 {
-                    LoginIdentity = item.LoginIdentity,
-                    Password = item.Password,
-                    Name = item.Name,
-                    Email = item.Email
 
-                };
+                    var model = new UserViewModel()
+                    {
+                        LoginIdentity = item.LoginIdentity,
+                        Password = item.Password,
+                        Name = item.Name,
+                        Email = item.Email
 
-                list.Add(model);
+                    };
+
+                    list.Add(model);
+                }
             }
 
 
@@ -49,73 +52,77 @@ namespace PauFacultyPortal.Service.Auth
 
         public UserViewModel CheckUserStatus(UserPasswordForgetViewModel data)
         {
-            UserViewModel result = new UserViewModel();
-            if (data != null && !string.IsNullOrEmpty(data.Email))
-            {
-                var user = from act in _db.Accounts.Where(x => x.AccountsRoleId == 5 && x.Deactivate == false &&
-                           x.Email == data.Email && x.LoginIdentity == data.LoginIdentity)
-                           select act;
-
-
-                result = new UserViewModel()
-                {
-                    Email = user.FirstOrDefault().Email,
-                    LoginIdentity= user.FirstOrDefault().LoginIdentity,
-                    Name = user.FirstOrDefault().Name,
-                    Password= user.FirstOrDefault().Password,
-                    LoginTime= DateTime.Now.ToString()
-
-                };
-            }
+            var result = UserValidation(data.Email, data.LoginIdentity);
             return result;
+
         }
 
         public int ResetUserAuth(UserPasswordResetViewModel modifiedTeacher)
         {
             int result = 0;
-            var entity = from act in _db.Accounts.Where(x => x.AccountsRoleId == 5 && x.Deactivate == false &&
-                            x.Email == modifiedTeacher.Email && x.LoginIdentity == modifiedTeacher.LoginIdentity) select act;
-            if (entity != null)
+            using (var db = new PauFacultyPortalEntities())
             {
-                entity.FirstOrDefault().Password = modifiedTeacher.Password;
-                entity.FirstOrDefault().LastPsswordChange = DateTime.Now.ToString();
-                result = _db.SaveChanges();
+                var entity = from act in db.Accounts.Where(x => x.AccountsRoleId == 5 && x.Deactivate == false &&
+                           x.Email == modifiedTeacher.Email && x.LoginIdentity == modifiedTeacher.LoginIdentity)
+                             select act;
+                if (entity != null)
+                {
+                    entity.FirstOrDefault().Password = modifiedTeacher.Password;
+                    entity.FirstOrDefault().LastPsswordChange = DateTime.Now.ToString();
+                    result = db.SaveChanges();
+                }
             }
+
             return result;
         }
 
         public UserViewModel CheckUserStatus(UserPasswordResetViewModel data)
         {
-            UserViewModel result = new UserViewModel();
-            if (data != null && !string.IsNullOrEmpty(data.Email))
-            {
-                var user = from act in _db.Accounts.Where(x => x.AccountsRoleId == 5 && x.Deactivate == false &&
-                           x.Email == data.Email && x.LoginIdentity == data.LoginIdentity)
-                           select act;
-
-
-                result = new UserViewModel()
-                {
-                    Email = user.FirstOrDefault().Email,
-                    LoginIdentity = user.FirstOrDefault().LoginIdentity,
-                    Name = user.FirstOrDefault().Name,
-                    Password = user.FirstOrDefault().Password,
-                    LoginTime = DateTime.Now.ToString()
-
-                };
-            }
+            var result = UserValidation(data.Email, data.LoginIdentity);
             return result;
+        }
+
+        private UserViewModel UserValidation(string email, string loginId)
+        {
+            UserViewModel result = new UserViewModel();
+
+            if (!string.IsNullOrEmpty(loginId) && !string.IsNullOrEmpty(email))
+            {
+                using (var db = new PauFacultyPortalEntities())
+                {
+                    var user = from act in db.Accounts.Where(x => x.AccountsRoleId == 5 && x.Deactivate == false &&
+                               x.Email == email && x.LoginIdentity == loginId)
+                               select act;
+
+
+                    result = new UserViewModel()
+                    {
+                        Email = user.FirstOrDefault().Email,
+                        LoginIdentity = user.FirstOrDefault().LoginIdentity,
+                        Name = user.FirstOrDefault().Name,
+                        Password = user.FirstOrDefault().Password,
+                        LoginTime = DateTime.Now.ToString()
+
+                    };
+                }
+
+
+            }
+
+
+            return result;
+
         }
 
         public void SendMail(UserViewModel checkUser)
         {
-          
+
             string subject = "User LoginId and Password";
             string body = "Dear " + checkUser.Name + ", " + Environment.NewLine
                            + "Your LoginId: " + checkUser.LoginIdentity + ",Password: " + checkUser.Password + Environment.NewLine + "Thanks," + Environment.NewLine + "UMS(Primeasia University)";
-                           
-            string fromMail = "sydul.hassan@primeasia.edu.bd";
-            string emailTo = "mamun.ruet@primeasia.edu.bd";
+
+            string fromMail = "ums@primeasia.edu.bd";
+            string emailTo = "husneara_asma@yahoo.com";
 
             MailMessage mail = new MailMessage();
             string host = "smtp.gmail.com";
@@ -125,40 +132,43 @@ namespace PauFacultyPortal.Service.Auth
             mail.To.Add(emailTo);
             mail.Subject = subject;
             mail.Body = body;
-            server.Credentials = new NetworkCredential("sydul.hassan@primeasia.edu.bd","30091991shr");
+            server.Credentials = new NetworkCredential("ums@primeasia.edu.bd", "primeasia123");
             server.EnableSsl = true;
             server.Send(mail);
-           
 
-          
+
+
         }
 
         public UserViewModel GetUserInfo(string Identity, string Password)
         {
 
             UserViewModel model = new UserViewModel();
+
             if (!string.IsNullOrEmpty(Identity) && !string.IsNullOrEmpty(Password))
             {
-                var result = (from act in _db.Accounts.Where(x => (x.AccountsRoleId == 1 || x.AccountsRoleId == 5) && x.Deactivate == false && x.LoginIdentity == Identity && x.Password == Password)
-                              select new
-                              {
-                                  LoginIdentity = act.LoginIdentity,
-                                  Password = act.Password,
-                                  Name = act.Name,
-                                  Email = act.Email,
-
-
-                              }).FirstOrDefault();
-
-                model = new UserViewModel()
+                using (var db = new PauFacultyPortalEntities())
                 {
-                    LoginIdentity = result.LoginIdentity,
-                    Password = result.Password,
-                    Name = result.Name,
-                    Email = result.Email
+                    var result = (from act in db.Accounts.Where(x => (x.AccountsRoleId == 1 || x.AccountsRoleId == 5) && x.Deactivate == false && x.LoginIdentity == Identity && x.Password == Password)
+                                  select new
+                                  {
+                                      LoginIdentity = act.LoginIdentity,
+                                      Password = act.Password,
+                                      Name = act.Name,
+                                      Email = act.Email,
 
-                };
 
+                                  }).FirstOrDefault();
+
+                    model = new UserViewModel()
+                    {
+                        LoginIdentity = result.LoginIdentity,
+                        Password = result.Password,
+                        Name = result.Name,
+                        Email = result.Email
+
+                    };
+                }
 
             }
 
